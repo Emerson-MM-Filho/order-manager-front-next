571f430408f96bb01f15b650686da0a61d23c6e1
Emerson Filho <emerson.dev.machado@gmail.com>
2022-07-03 03:05:18 -0300

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,29 +1,10 @@
 import type { NextPage } from 'next'
+import LoginWithGoogle from '../src/components/login_with_google'
 
 const Login: NextPage = () => {
   return (
     <section>
-      <script src="https://accounts.google.com/gsi/client" async defer></script>
-
-      <section className='social-login-section' style={{
-        'width': '25%'
-      }}>
-        <div
-          id="g_id_onload"
-          data-client_id="946242776728-hetk06u45drp07qrgohj4hbciq417p34.apps.googleusercontent.com"
-          data-login_uri="https://order-manager-front.herokuapp.com"
-          data-auto_prompt="false">
-        </div>
-        <div
-          className="g_id_signin"
-          data-type="standard"
-          data-size="large"
-          data-theme="filled_blue"
-          data-text="continue_with"
-          data-shape="rectangular"
-          data-logo_alignment="left">
-        </div>
-      </section>
+      <LoginWithGoogle/>
     </section>
   )
 }
