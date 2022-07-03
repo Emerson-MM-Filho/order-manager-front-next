12115ce80b9c56eb4c203b40cdbc6609c80f4672
Emerson Filho <emerson.dev.machado@gmail.com>
2022-07-03 02:39:42 -0300

    google login

---
+++ pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Dashboard: NextPage = () => {
+  return (
+    <section>
+      Dashboard
+    </section>
+  )
+}
+
+export default Dashboard
--- pages/login.tsx
+++ pages/login.tsx
@@ -11,15 +11,15 @@ const Login: NextPage = () => {
         <div
           id="g_id_onload"
           data-client_id="946242776728-hetk06u45drp07qrgohj4hbciq417p34.apps.googleusercontent.com"
-          data-login_uri="http://localhost:3000"
+          data-login_uri="https://order-manager-front.herokuapp.com"
           data-auto_prompt="false">
         </div>
         <div
           className="g_id_signin"
           data-type="standard"
           data-size="large"
-          data-theme="outline"
-          data-text="sign_in_with"
+          data-theme="filled_blue"
+          data-text="continue_with"
           data-shape="rectangular"
           data-logo_alignment="left">
         </div>
